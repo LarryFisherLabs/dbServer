1ae17abbdd9956ad82e2845c155013a14a5b96ab
LarryFisherLabs <117693332+LarryFisherLabs@users.noreply.github.com>
2023-02-28 17:25:13 -0500

    Update routes.js
    Add support for multiple networks and metadata calls

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getAntImage } from "../controllers/ants.js";
-import { getCoinImage } from "../controllers/coin.js";
+import { getAnt, getAntImage } from "../controllers/ants.js";
+import { getCoin, getCoinImage } from "../controllers/coin.js";
 
 export const router = express.Router(); 
 
-router.get('/coins/images/:id', getCoinImage);
-router.get('/ants/images/:id', getAntImage);
+// !!! network ids !!!
+// 0 sepolia
+// 1 goerli
+
+router.get('/:netId/coins/images/:id', getCoinImage);
+router.get('/:netId/ants/images/:id', getAntImage);
+router.get('/:netId/coins/:id', getCoin);
+router.get('/:netId/ants/:id', getAnt);
